67f914c2d5458f4bbb3f0dfde9d3fa1ddd2dff25
SvetlanaKhazheeva <Svetlana82-31@mail.ru>
2023-05-09 02:49:57 +0300

    ДЗ по вебинару 3 выполнено

--- homework3/task2.py
+++ homework3/task2.py
@@ -8,7 +8,7 @@ def get_list_info(lst):
     min_elem = min(lst)
     max_elem = max(lst)
     sum_list = sum(lst)
-    average = round(mean(lst), 2)  # Здесь нужно написать код
+    average = round(mean(lst), 2)  
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
